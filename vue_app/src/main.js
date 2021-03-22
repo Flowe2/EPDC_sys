9b252cf12a49ecca0e42497f37c4f3136a12eb20
Huaqin Kong <kong954827899@vip.qq.com>
2021-03-22 21:55:06 +0800

    init vue-router

--- vue_app/src/main.js
+++ vue_app/src/main.js
@@ -4,7 +4,4 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router'
 
-// import Vue from 'vue';
-// Vue.use(VueAxios, axios);
-
 createApp(App).use(router).use(VueAxios, axios).mount('#app')
---
+++ vue_app/src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <userLogin></userLogin>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import userLogin from '@/components/userLogin.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    userLogin
+  }
+}
+</script>
